9bda46773edb527e30ab1962e2b1e65f91c78fc8
myweb768 <myweb768@gmail.com>
2026-05-27 15:05:36 +0600

    Add JS Functionality to make dynamic Time and Form

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("Hello World!");
+
+
+// JS code to handle the form visibility
+let addExpenseBtn = document.getElementById("addExpenseBtn");
+let expenseForm = document.querySelector(".ExpenseForm");
+let closeFormBtn = document.getElementById("closeForm");
+
+addExpenseBtn.addEventListener("click", () => {
+      expenseForm.classList.add("fixed");
+      expenseForm.classList.remove("hidden");
+});
+
+closeFormBtn.addEventListener("click", () => {
+        expenseForm.classList.remove("fixed");
+        expenseForm.classList.add("hidden");
+});
+
+//------------------------------------------------
+
+// JS Clock Code 
+function updateClock() {
+    const now = new Date();
+    const timeString = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true });
+    const dateString = now.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
+
+    document.getElementById('myDate').textContent = dateString;
+    document.getElementById('myClock').textContent = timeString;
+}
+
+setInterval(updateClock, 1000);
+updateClock(); 
+//--------------------------------------------- 
