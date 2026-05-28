6505e340b99fbc852d410d581a9d5330855d5001
myweb768 <myweb768@gmail.com>
2026-05-28 22:25:25 +0600

    Update JS for form data submittion and velidation

--- script.js
+++ script.js
@@ -31,3 +31,53 @@ function updateClock() {
 setInterval(updateClock, 1000);
 updateClock(); 
 //--------------------------------------------- 
+
+// // JS code to handle the form submission
+
+const expenses = [];
+const formTag = document.querySelector(".ExpenseForm form");
+let expenseInput = document.querySelector("#title");
+let amountInput = document.querySelector("#amount");
+let dateInput = document.querySelector("#date");
+let expenseTable  = document.getElementById("expenseTableBody");
+
+function renderExpenses(){
+    expenseTable.innerHTML = "";
+    expenses.forEach((expense)=>{
+        const tr = document.createElement("tr");
+        tr.className = "border-b hover:bg-gray-100 transition duration-150";
+        tr.innerHTML = `
+        <td class="p-2 text-center text-gray-800">${expense.title}</td>
+            <td class="p-2 text-center text-red-600 font-semibold">৳${expense.amount}</td>
+            <td class="p-2 text-center text-gray-500">${expense.date}</td>
+        `;
+        expenseTable.appendChild(tr);
+    
+    });
+}
+
+formTag.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const title = expenseInput.value.trim();
+    const amount = parseFloat(amountInput.value);
+    const date = dateInput.value;
+
+    if(title === "" || isNaN(amount)|| amount <= 0 || date === ""){
+        alert("Please fill in all fields with valid data.");
+        return;
+    }
+
+    const newExpense = {
+       id: Date.now(),
+       title: title,
+       amount: amount,
+       date: date
+    };
+
+    expenses.push(newExpense);
+    renderExpenses();
+    formTag.reset();
+    expenseForm.classList.add("hidden");
+    expenseForm.classList.remove("fixed");
+
+});
